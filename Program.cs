f4daed86a4508d4362379bd0b4db919c44d26b60
Khoa Nguyen <khoanguyen0497@gmail.com>
2024-08-14 16:01:35 +1200

    stuff

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using CitizenshipCalculator;
-
+using System.Diagnostics.Metrics;
+//https://fyi.org.nz/request/18902-what-the-term-each-12-month-period-in-the-presence-requirement-having-been-in-new-zealand-for-at-least-240-days-in-each-12-month-period-in-the-last-5-years-means-exactly-for-citizenship-by-grant
 List<TravelRecord> travelRecords = new List<TravelRecord>();
 
 bool calculateAlternative240Rule = false;
@@ -9,22 +10,44 @@ int TOTAL_DAYS = 1350; // 1350 days in 5 years
 
 DateTime START_RESIDENCY = new DateTime(2022, 04, 26);
 DateTime CITIZENSHIP_DATE = START_RESIDENCY.AddYears(5); // normal route 5 whole years
-CITIZENSHIP_DATE = START_RESIDENCY.AddYears(5).AddDays(-30); // fast route, including days before cizitenship   
-travelRecords.Add(new TravelRecord(CITIZENSHIP_DATE.AddYears(-5), START_RESIDENCY.AddDays(1))); // any day before residency is day travelled
+CITIZENSHIP_DATE = CITIZENSHIP_DATE.AddDays(0); // fast route, including days before cizitenship   
+
+
+Console.WriteLine("---------------------");
+Console.WriteLine($"{CITIZENSHIP_DATE.ToString(dateFormat)}");
+Console.WriteLine("---------------------");
+Console.WriteLine();
 
 travelRecords.Add(new TravelRecord(new DateTime(2022, 12, 14), new DateTime(2023, 01, 04))); // have travelled
 travelRecords.Add(new TravelRecord(new DateTime(2023, 07, 06), new DateTime(2023, 08, 12))); // have travelled
+travelRecords.Add(new TravelRecord(new DateTime(2023, 12, 18), new DateTime(2024, 02, 24))); // have travelled
 
-travelRecords.Add(new TravelRecord(new DateTime(2023, 12, 10), new DateTime(2024, 02, 20)));
-travelRecords.Add(new TravelRecord(new DateTime(2024, 06, 11), new DateTime(2024, 08, 09)));
-travelRecords.Add(new TravelRecord(new DateTime(2024, 12, 20), new DateTime(2025, 02, 10)));
-travelRecords.Add(new TravelRecord(new DateTime(2025, 06, 10), new DateTime(2025, 08, 10)));
-travelRecords.Add(new TravelRecord(new DateTime(2025, 12, 20), new DateTime(2026, 02, 10)));
-travelRecords.Add(new TravelRecord(new DateTime(2026, 06, 10), new DateTime(2026, 08, 10)));
-//travelRecords.Add(new TravelRecord(new DateTime(2026, 12, 20), new DateTime(2027, 02, 10)));
 
-int totalDaysAllowedToTravel = (CITIZENSHIP_DATE - CITIZENSHIP_DATE.AddYears(-5)).Days - TOTAL_DAYS;
-int totalDaysTravelled = travelRecords.Sum(x => x.DaysTravelled());
+travelRecords.Add(new TravelRecord(new DateTime(2024, 09, 20), new DateTime(2024, 10, 31))); // booked
+
+travelRecords.Add(new TravelRecord(new DateTime(2025, 1, 10), new DateTime(2025, 02, 20)));
+
+travelRecords.Add(new TravelRecord(new DateTime(2025, 06, 10), new DateTime(2025, 08, 4)));
+travelRecords.Add(new TravelRecord(new DateTime(2025, 12, 20), new DateTime(2026, 02, 13)));
+travelRecords.Add(new TravelRecord(new DateTime(2026, 06, 10), new DateTime(2026, 08, 4)));
+travelRecords.Add(new TravelRecord(new DateTime(2026, 12, 20), new DateTime(2027, 02, 13)));
+
+
+
+//travelRecords.Add(new TravelRecord(new DateTime(2027, 7, 10), new DateTime(2027, 08, 20)));
+
+
+
+
+int totalDaysAllowedToTravel = (CITIZENSHIP_DATE.AddDays(-1) - CITIZENSHIP_DATE.AddYears(-5)).Days + 1 - TOTAL_DAYS;
+int totalDaysTravelled = travelRecords.Sum(x => x.DaysTravelledWithinPeriod(CITIZENSHIP_DATE.AddYears(-5), CITIZENSHIP_DATE.AddDays(-1)));
+
+if (CITIZENSHIP_DATE.AddYears(-5) < START_RESIDENCY)
+{
+    totalDaysTravelled += (START_RESIDENCY - CITIZENSHIP_DATE.AddYears(-5)).Days;
+}
+
+
 int totalDaysLeftToTravel = totalDaysAllowedToTravel - totalDaysTravelled;
 
 // calculation for days travelled and 1350 rule
@@ -40,34 +63,42 @@ Console.WriteLine($"Total days allowed: {totalDaysAllowedToTravel}");
 Console.WriteLine($"Days travelled: {totalDaysTravelled}");
 Console.WriteLine($"Days left to travel: {totalDaysLeftToTravel}");
 
-decimal numberOf30DayPeriods = (CITIZENSHIP_DATE - travelRecords.Last().End).Days / 30m;
-Console.WriteLine($"Average days to travel PER 30-DAY PERIOD left: {Math.Round(totalDaysLeftToTravel / numberOf30DayPeriods, 2)}");
-
-decimal numberOf356DayPeriods = (CITIZENSHIP_DATE - travelRecords.Last().End).Days / 365m;
-Console.WriteLine($"Average days to travel PER 365-DAY PERIOD left: {Math.Round(totalDaysLeftToTravel / numberOf356DayPeriods, 2)}");
-
 Console.WriteLine();
 
 // Calculate rule 240. This assumes 5 12-month periods, and not ANY 12-month period
-Console.WriteLine("240 RULE CONDITIONS");
+Console.WriteLine("240 RULE CONDITIONS COUNTING BACKWARDS");
 
-DateTime startPeriod = CITIZENSHIP_DATE.AddYears(-5);
-DateTime endPeriod = startPeriod.AddYears(1);
+DateTime startPeriod = CITIZENSHIP_DATE.AddYears(-5).AddDays(0);
+DateTime endPeriod = startPeriod.AddYears(1).AddDays(-1);
 
 int totalDaysTravelledUptoPeriod = 0;
 while (true)
 {
     int daysTravelledThisPeriod = 0;
+    int tripsThisPeriod = 0;
     foreach (var record in travelRecords)
     {
-        daysTravelledThisPeriod += record.DaysTravelledWithinPeriod(startPeriod, endPeriod);
+        var travelledDays = record.DaysTravelledWithinPeriod(startPeriod, endPeriod);
+        daysTravelledThisPeriod += travelledDays;
+        if (travelledDays > 0)
+        {
+            tripsThisPeriod++;
+        }
+    }
+
+    if (startPeriod < START_RESIDENCY)
+    {
+       daysTravelledThisPeriod += (START_RESIDENCY - startPeriod).Days;
+
     }
 
     totalDaysTravelledUptoPeriod += daysTravelledThisPeriod;
-    int daysAllowedThisPeriod = (endPeriod - startPeriod).Days - SHORT_PERIOD_DAYS;
+    int daysThisPeriod = (endPeriod - startPeriod).Days + 1;
+    int daysAllowedThisPeriod = daysThisPeriod - SHORT_PERIOD_DAYS;
     int daysLeftToTravel = Math.Min(totalDaysAllowedToTravel - totalDaysTravelledUptoPeriod, daysAllowedThisPeriod - daysTravelledThisPeriod);
+    
 
-    Console.WriteLine($"Period {startPeriod.ToString(dateFormat)} - {endPeriod.ToString(dateFormat)}. Days travelled: {daysTravelledThisPeriod}. Days left to travel: {daysLeftToTravel}");
+    Console.WriteLine($"Period {startPeriod.ToString(dateFormat)} - {endPeriod.ToString(dateFormat)}. Days in period: {daysThisPeriod}. Days travelled this period: {daysTravelledThisPeriod}. Days left to travel: {daysLeftToTravel}. Trips in this period (incl. partial trips): {tripsThisPeriod}");
     startPeriod = endPeriod.AddDays(1);
     endPeriod = startPeriod.AddYears(1).AddDays(-1);
 
@@ -77,37 +108,40 @@ while (true)
     }
 }
 
-// just in case, calculate any 12-month period
-startPeriod = CITIZENSHIP_DATE.AddYears(-5);
-endPeriod = startPeriod.AddYears(1);
-
-
-
-while (true)
+int DaysInNZYear(DateTime d1, DateTime end)
 {
-    if (!calculateAlternative240Rule)
-    {
-        break;
-    }
-
-    if (endPeriod > CITIZENSHIP_DATE)
-    {
-        break;
-    }
-
-    int daysTravelledThisPeriod = 0;
-    int daysAllowedThisPeriod = (endPeriod - startPeriod).Days - SHORT_PERIOD_DAYS;
-
-    foreach (var record in travelRecords)
-    {
-        daysTravelledThisPeriod += record.DaysTravelledWithinPeriod(startPeriod, endPeriod);
-    }
+    int days = 0;
     
-    if (daysTravelledThisPeriod > daysAllowedThisPeriod)
+    while (d1 <= end)
     {
-        Console.WriteLine($"ERROR: too many days travelled {startPeriod.ToString(dateFormat)} - {endPeriod.ToString(dateFormat)}. Days {daysTravelledThisPeriod}. Allowed {daysAllowedThisPeriod}");
+        if (d1 < START_RESIDENCY) 
+        { 
+            
+        }
+        else if (travelRecords.All(x => x.InNZ(d1)))
+        {
+            days++;
+        }
+        d1=d1.AddDays(1);
     }
 
-    startPeriod = startPeriod.AddDays(1);
-    endPeriod = startPeriod.AddYears(1);
+    return days;
 }
+Console.WriteLine();
+
+int startYear = -5;
+int totalDaysinNZ = 0;
+while (startYear < 0)
+{
+    DateTime start = CITIZENSHIP_DATE.AddYears(startYear);
+    DateTime end = start.AddYears(1).AddDays(-1);
+    int days = DaysInNZYear(start, end);
+    totalDaysinNZ += days;
+    Console.WriteLine($"Period {start.ToString(dateFormat)} - {end.ToString(dateFormat)}. Days in NZ - {days}. Days to travel: {days - SHORT_PERIOD_DAYS}");
+    startYear++;
+}
+Console.WriteLine($"Days total in NZ: {totalDaysinNZ}");
+
+
+DateTime test1 = new DateTime(2022, 04, 22);
+DateTime test2 = test1.AddYears(1);
--- TravelRecord.cs
+++ TravelRecord.cs
@@ -8,19 +8,31 @@ namespace CitizenshipCalculator
 {
     public class TravelRecord
     {
-        public TravelRecord(DateTime start, DateTime end)
+        public TravelRecord(DateTime start, DateTime end, string note = null)
         {
             Start = start;
             End = end;
+            Note = note;
         }
 
         public DateTime Start { get; set; }
 
         public DateTime End { get; set; }
+        public string Note { get; set; }
+
+        public bool InNZ(DateTime d)
+        {
+            if (d > Start && d < End)
+            {
+                return false;
+            }
+
+            return true;
+        }
 
         public int DaysTravelled()
         {
-            return (End - Start).Days + 1; // Inclusive of all days
+            return (End - Start).Days - 1;
         }
 
         public int DaysTravelledWithinPeriod(DateTime periodStart, DateTime periodEnd)
@@ -38,13 +50,25 @@ namespace CitizenshipCalculator
             DateTime tmpStart = periodStart >= Start ? periodStart : Start;
             DateTime tmpEnd = periodEnd >= End ? End : periodEnd;
 
-            return (tmpEnd - tmpStart).Days + 1;
+            int days =  (tmpEnd - tmpStart).Days - 1;
+
+            if (periodStart > Start)
+            {
+                days++;
+            }
+
+            if (periodEnd < End)
+            {
+                days++;
+            }
+
+            return days;
 
         }
 
         public override string ToString()
         {
-            return $"{Start:dd/MM/yyyy} - {End:dd/MM/yyyy}. Days {DaysTravelled()}";
+            return $"{Start:dd/MM/yyyy} - {End:dd/MM/yyyy}. Days {DaysTravelled()}. Note: {Note}";
         }
     }
 }
